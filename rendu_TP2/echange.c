c7416d234c7c306d39edcd1ea94b946a8c890ee8
MHAYA Nizar <nizar.mhaya@telecomnancy.eu>
2023-09-20 09:07:24 +0200

    Premier commit pour echange

---
+++ rendu_TP2/echange.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+void echangeContenu(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+int main() {
+    int a = 10;
+    int b = 20;
+
+    printf("Avant l'échange : a = %d, b = %d\n", a, b);
+
+    echangeContenu(&a, &b);
+
+    printf("Après l'échange : a = %d, b = %d\n", a, b);
+
+    return 0;
+}
